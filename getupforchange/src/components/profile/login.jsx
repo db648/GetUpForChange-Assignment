2fbd721e98b1f9fa66b82a2a5d8da90cd1b46301
db648 <95957492+db648@users.noreply.github.com>
2022-06-25 13:03:23 +0530

    profile page done

--- getupforchange/src/components/profile/login.jsx
+++ getupforchange/src/components/profile/login.jsx
@@ -1,4 +1,39 @@
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { loginSuccess } from "../redux/auth-redux/action";
+
 export const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const onSubmit = (data) => {
+    axios
+      .post("https://ecommerce-shopping-project.herokuapp.com/user/login", data)
+      .then((res) => {
+        console.log("before", res.data);
+        if (res.data.status === "false") {
+          return alert("Enter Correct Details");
+        } else {
+          dispatch(loginSuccess(res.data));
+          console.log("after", res.data);
+          setTimeout(() => {
+            navigate("/");
+          }, 1000);
+        }
+      })
+      .catch((err) => {
+        return alert("Enter Correct Details");
+      });
+  };
   return (
     <>
       <section className="h-100 h-custom">
@@ -6,16 +41,10 @@ export const Login = () => {
           <div className="row d-flex justify-content-center align-items-center h-100">
             <div className="col-lg-8 col-xl-6">
               <div className="card rounded-3">
-                {/* <img
-                  src="https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/banner-images/Homepage-Banner_3.jpg?format=webp&w=1366&dpr=1.0"
-                  className="w-100"
-                  alt="Sample photo"
-                /> */}
-
                 <div className="card-body p-4 p-md-5">
                   <h3 className="mb-5 pb-5 pb-md-0 mb-md-5 px-md-2">Sign In</h3>
 
-                  <form className="px-md-2">
+                  <form className="px-md-2" onSubmit={handleSubmit(onSubmit)}>
                     <div className="form-outline mb-4">
                       <label className="form-label" htmlFor="form3Example3cg">
                         Your Email
@@ -24,6 +53,8 @@ export const Login = () => {
                         type="email"
                         id="form3Example3cg"
                         className="form-control form-control-lg"
+                        {...register("email", { required: true })}
+                        required
                       />
                     </div>
 
@@ -35,6 +66,8 @@ export const Login = () => {
                         type="password"
                         id="form3Example4cg"
                         className="form-control form-control-lg"
+                        {...register("password", { required: true })}
+                        required
                       />
                     </div>
 
@@ -48,15 +81,11 @@ export const Login = () => {
                     </div>
 
                     <p className="text-center text-muted mt-5 mb-0">
-                      Create New Account here{" "}
-                      <button
-                        className="btn fw-bold text-body"
-                        onClick={() => {
-                          navigate("/register");
-                        }}
-                      >
-                        <u>Register here</u>
-                      </button>
+                    Create New Account here{" "}
+                      
+                            <button className="btn fw-bold text-body" onClick={() => {
+                                navigate("/register")
+                            }}><u>Register here</u></button>
                     </p>
                   </form>
                 </div>
@@ -65,6 +94,7 @@ export const Login = () => {
           </div>
         </div>
       </section>
+     
     </>
   );
 };
---
+++ getupforchange/src/components/profile/register.jsx
@@ -0,0 +1,109 @@
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
+export const Registerpage = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  
+  const navigate = useNavigate();
+
+  const onSubmit = (data) => {
+    console.log("submit", data);
+    axios
+      .post("https://ecommerce-shopping-project.herokuapp.com/user/register", data)
+      .then((res) => {
+        setTimeout(() => {
+          navigate("/login");
+        }, 1000);
+      })
+      .catch((err) => {
+        alert("Enter Correct Details");
+      });
+  };
+
+  return (
+    <>
+      <section className="h-100 h-custom">
+        <div className="container py-5 h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-lg-8 col-xl-6">
+              <div className="card rounded-3">
+                <img
+                  src="https://prod-img.thesouledstore.com/public/theSoul/uploads/slider/20220513000555.jpg?format=webp&w=1366&dpr=1.0"
+                  className="w-100"
+                  alt="Sample photo"
+                />
+
+                <div className="card-body p-4 p-md-5">
+                  <h2 className="mb-4 pb-2 pb-md-0 mb-md-5 px-md-2">
+                    Create New Account
+                  </h2>
+
+                  <form className="px-md-2" onSubmit={handleSubmit(onSubmit)}>
+                    <div className="form-outline mb-4">
+                      <label className="form-label" htmlFor="form3Example1cg">
+                        Your Name
+                      </label>
+                      <input
+                        type="text"
+                        id="form3Example1cg"
+                        className="form-control form-control-lg"
+                        {...register("username", { required: true })}
+                      />
+                    </div>
+
+                    <div className="form-outline mb-4">
+                      <label className="form-label" htmlFor="form3Example3cg">
+                        Your Email
+                      </label>
+                      <input
+                        type="email"
+                        id="form3Example3cg"
+                        className="form-control form-control-lg"
+                        {...register("email", { required: true })}
+                      />
+                    </div>
+
+                    <div className="form-outline mb-4">
+                      <label className="form-label" htmlFor="form3Example4cg">
+                        Password
+                      </label>
+                      <input
+                        type="password"
+                        id="form3Example4cg"
+                        className="form-control form-control-lg"
+                        {...register("password", { required: true })}
+                      />
+                    </div>
+
+                    <div className="d-flex justify-content-center">
+                      <button
+                        type="submit"
+                        className="btn btn-primary btn-lg btn-block"
+                      >
+                        Register
+                      </button>
+                    </div>
+
+                    <p className="text-center text-muted mt-5 mb-0">
+                      Have already an account?{" "}
+                      
+                            <button className="btn fw-bold text-body" onClick={() => {
+                                navigate("/login")
+                            }}><u>Login here</u></button>
+                    </p>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
